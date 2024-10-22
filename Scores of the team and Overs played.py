d11360c616815871fd94093f4aa51396991fa0cf
jayanth2341 <yjayanth769@gmail.com>
2024-10-22 10:02:00 +0530

    Scores of the team and Overs played.py

--- Scores of the team and Overs played.py
+++ Scores of the team and Overs played.py
@@ -1,5 +1,6 @@
 """
-3)It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School. It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
+It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School.
+It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
 Write a program to display the details of the match with Team name, Scores of the team and Overs played.
 Input and Output Format:  
 Refer sample input and output for formatting specifications.
