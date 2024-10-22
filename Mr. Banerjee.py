56f9447ac12fd2b863483da4e1a5c1f1524a374a
jayanth2341 <yjayanth769@gmail.com>
2024-10-22 10:01:30 +0530

    Update Mr. Banerjee.py

--- Mr. Banerjee.py
+++ Mr. Banerjee.py
@@ -1,5 +1,5 @@
 """
-2)After Dhoni made it to the pinnacle of Success as Indian Captain, Mr. Banerjee was once invited by the Media to recall his association with Dhoni.
+After Dhoni made it to the pinnacle of Success as Indian Captain, Mr. Banerjee was once invited by the Media to recall his association with Dhoni.
 Mr. Banerjee quoted one important moment that complemented the traits of Dhoni.
 It was when he approached Dhoni and asked him if he would play cricket for the school team saying "Will you be a wicketkeeper?" and was taken aback by Dhoni's confident reply "I will if I get a chance".
 Write a program to display the answers of Dhoni that impressed his master.
